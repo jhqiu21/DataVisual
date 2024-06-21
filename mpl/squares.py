e9eac86b16b92e4e73d5151b1118389a8374f9f8
QIU JINHANG <142405039+jhqiu21@users.noreply.github.com>
2024-06-21 14:32:34 +0800

    update squares.py

--- mpl/squares.py
+++ mpl/squares.py
@@ -1,7 +1,27 @@
 import matplotlib.pyplot as plt
 
-squares = [1, 4, 8, 9, 16, 10]
-fig, ax = plt.subplots()
-ax.plot(squares)
+# input = [1, 2, 3, 4, 5]
+# output = [1, 4, 9, 16, 25]
+ 
+x_value = range(1, 1001)
+y_value = [x ** 2 for x in x_value]
 
-plt.show
+
+plt.style.use('seaborn-v0_8')
+
+fig, ax = plt.subplots()
+# ax.plot(input, output, linewidth = 3)
+
+# ax.scatter(input, output, s = 100)
+ax.scatter(x_value, y_value, s = 5)
+ax.axis([0, 1100, 0, 1100000])
+
+# set the title and x,y-axis of the figure
+ax.set_title("Title", fontsize = 24)
+ax.set_xlabel("Value", fontsize = 14)
+ax.set_ylabel("y-axis", fontsize = 14)
+
+# set the fontsize of x,y-label of the figure
+ax.tick_params(axis = 'both', which = 'major',labelsize = 14)
+
+plt.show()
