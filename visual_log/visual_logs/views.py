84377f231848599758693616823e029dfa8aa586
QIU JINHANG <142405039+jhqiu21@users.noreply.github.com>
2024-06-22 16:28:18 +0800

    associate users to their own data

--- visual_log/visual_logs/views.py
+++ visual_log/visual_logs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, Http404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -6,30 +8,41 @@ from .forms import TopicForm, EntryForm
 def index(request):
     return render(request, 'visual_logs/index.html')
 
+@login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'visual_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'visual_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
     else:
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save()
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('visual_logs:topics')
     context = {'form': form}
     return render(request, 'visual_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     if request.method != 'POST':
         form = EntryForm()
     else:
@@ -43,6 +56,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'visual_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
