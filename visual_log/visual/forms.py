fbb4f628da07e8c4b2011d334ddb207c155626d0
QIU JINHANG <142405039+jhqiu21@users.noreply.github.com>
2024-06-24 21:36:17 +0800

    add upload ui and flex some bugs

--- visual_log/visual/forms.py
+++ visual_log/visual/forms.py
@@ -3,8 +3,22 @@ from .models import UploadedFile
 
 # upload file form
 class UploadFileForm(forms.ModelForm):
-    file = forms.FileField()
+    file = forms.FileField(
+        widget=forms.ClearableFileInput(attrs={
+            'class': 'custom-file-input',
+            'id': 'customFile'
+        })
+    )
+    title = forms.CharField(
+        min_length=0,
+        max_length=50,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter title here'
+        })
+    )
+
     class Meta:
         model = UploadedFile
-        fields = ['file']
+        fields = ['file', 'title']
         
