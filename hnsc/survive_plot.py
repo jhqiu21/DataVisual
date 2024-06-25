b4f03b77adbf10f248fe6ac19f2e0403d415d882
QIU JINHANG <142405039+jhqiu21@users.noreply.github.com>
2024-06-25 10:32:33 +0800

    add example and test data set

--- hnsc/survive_plot.py
+++
@@ -1,9 +0,0 @@
-# generate survive plot for a given json data set
-import json
-
-filename = 'hnsc/clinical.project-tcga-hnsc.2024-06-21.json'
-with open(filename) as f:
-    all_tcga_data = json.load(f)
-
-
-
