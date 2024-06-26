eb4270772e6528ea7e64001c1b397916f1602b3d
QIU JINHANG <142405039+jhqiu21@users.noreply.github.com>
2024-06-26 20:35:49 +0800

    add plot method

--- visual_log/visual/scripts/line.py
+++ visual_log/visual/scripts/line.py
@@ -2,32 +2,30 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
+from datetime import datetime
 
-def cumulative_distributions(data):
+def line_chart(data):
     # initialize figure
     plt.style.use(data.style)
     fig, ax = plt.subplots(dpi=360)
     title = data.plot_title
     error_msg = ["Error!"]
     try:
-        paraXs = data.para_data.get('datas')
+        paraXs = data.para_data.get('paraX_datas')
+        paraYs = data.para_data.get('paraY_datas')
         missing_msg = data.para_data.get('message')
     except IndexError:
         error_msg.append("You input an invalid parameter!")
         return {'figure': fig, 'message': error_msg}
 
-    ax.ecdf(paraXs[0], label="CDF")
-    ax.hist(paraXs[0], data.bin, density=True, histtype="step",
-            cumulative=True, label="Cumulative histogram")
+    ax.plot(paraXs[0], paraYs[0], linewidth=2)
     
     # Set the title and labels
-    ax.grid(True)
-    ax.legend()
-    ax.label_outer()
     ax.set_title(title, fontsize=data.plot_title_font)
     ax.set_xlabel(data.x_title, fontsize=data.x_font)
     ax.set_ylabel(data.y_title, fontsize=data.y_font)
 
+    # pack result information
     result = {'figure': fig, 'message': missing_msg}
     return result
 
